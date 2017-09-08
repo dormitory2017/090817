034ca2c0c771ffa1d7bce3a508e259bee821dc2d
Simple_pig <597937967@qq.com>
2017-09-08 15:53:54 +0800

    轮播图修改

--- js/new_file.js
+++ js/new_file.js
@@ -15,23 +15,54 @@ $(document).ready(function(){
 	
 	//轮播图
 	var banner =  $(".banner");
-	var ul = $(".banner ul");
-	var count = $(".banner li").length;
+	var images = $(".banner a")
 	var index = 0;
-	
-	var width = $(".banner").width();
-	var height = $(".banner").height();
-	
-	var points = $(".points");
+	var count = images.length;
 	
 	
+	//仅显示第一张图    eq(0)为第一张图   隐藏所有图片 .显示第一张图
+	images.hide().eq(0).show();
+	
+	//添加圆点
+	for(var i = 0; i<count;i++){
+		$(".points").append("<span></span>") ;//append是添加元素   addClass是添加类
+	}
+	
+	var points = $(".points span");// 获取小圆点
+	$(".points").css("margin-left",(-1*20)*count+"px");
+	points.eq(0).addClass("active");//为第一张图片默认添加一个.active类
+	//设定定时器
+	var t = setInterval(rolling,1500);
+		
+	//添加hover效果
+	
+	points.hover(function(){
+		var i = $(this).index();
+		if(i!=index){
+			images.eq(index).fadeOut(200);
+			points.eq(index).removeClass("active");
+		}
+		index = i;
+		images.eq(index).fadeIn(200);
+		points.eq(index).addClass("active");
+	});
 	
 	
+	//有hover操作时停止定时器
+	
+	banner.hover(function(){
+		clearInterval(t);
+	},function(){
+		setInterval(rolling,1500);
+	});
 	
 	
-	//
-	
-	
-	
+	function rolling(){
+		images.eq(index).fadeOut(400);   //第eq(index)个图片  在400毫秒内显示
+		points.eq(index).removeClass("active");//移除第eq(index)个图片的"active"属性
+		index = (index+1)%count;//index(索引值) 在count内循环  语句位置在中间
+		images.eq(index).fadeIn(400);
+		points.eq(index).addClass("active");
+	}
 	
 });
