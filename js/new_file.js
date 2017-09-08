1bd10a403167e37516b0623174011c30b7ad5258
Simple_pig <597937967@qq.com>
2017-09-08 14:11:31 +0800

    修改1

---
+++ js/new_file.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	//下拉菜单
+	$("nav ul li").hover(function(){
+		$(this).next("ul").slideDown(300);
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//轮播图
+	var banner =  $(".banner");
+	var ul = $(".banner ul");
+	var count = $(".banner li").length;
+	var index = 0;
+	
+	var width = $(".banner").width();
+	var height = $(".banner").height();
+	
+	var points = $(".points");
+	
+	
+	
+	
+	
+	
+	//
+	
+	
+	
+	
+});
